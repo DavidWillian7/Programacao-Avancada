7fcff458e7a436bfb98a780c4bd934985d6e2a0e
DavidWillian7 <85882885+DavidWillian7@users.noreply.github.com>
2021-06-26 15:17:14 -0300

    Update Qestao12.c

--- Lista-de-Ponteiros-em-C/Questão-12/Qestao12.c
+++ Lista-de-Ponteiros-em-C/Questão-12/Qestao12.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int soma(int a, int b)
+int sub(int a, int b)
 {
-  return a+b;
+  return a-b;
 }
 
 int main() 
 {
-  int (*pf)(int,int) = &soma;
+  int (*pf)(int,int) = &sub;
   printf("%d\n",pf(3,3));
   return 0;
 }
